e8eda6105e209d1590a08b3ddb5ba16e274cc50e
zxliver <10804941+zxliver@user.noreply.gitee.com>
2022-10-06 21:54:55 +0800

    promise

--- src/views/JavaScript/generator.tsx
+++ src/views/JavaScript/generator.tsx
@@ -21,10 +21,35 @@ const Index: FC = () => {
       scheduler(task);
     }
   }
+
+  const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('failed')
+      // resolve('success')
+    }, 2000)
+  })
+
+  const testReject = () => {
+    promise1.then(res => console.log(res)
+    ).catch(err => console.log('error:' + err))
+  }
+  // 测试 return 返回值 === resolve
+  const testReturn = () => {
+    // promise1.then(res => ({ code: 200, num: 1 })).then(res => console.log(res))
+    promise1.then(res => {
+      return new Promise((resolve, reject) => {
+        resolve({ code: 201, num: 1 })
+        reject({ code: 404, num: 1 })
+      })
+    }).then(res => console.log(res)).catch(error => console.log('error' + error)
+    ).finally(() => console.log('finally.....'))
+  }
   return (
     <>
       <h1> generator </h1>
       <button className={styles.btn1} onClick={() => scheduler(someTask(1))}>测试generator</button>
+      <button onClick={testReject}>reject</button>
+      <button onClick={testReturn}>return === promise</button>
     </>
   )
 }
